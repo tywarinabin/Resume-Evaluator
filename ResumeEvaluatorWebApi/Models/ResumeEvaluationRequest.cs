ab87441acaaec6672a2c80a2b0f7248be34082e4
Nabin Tiwari <nabintywari111@gmail.com>
2025-09-24 15:00:32 +0530

    Initial Commit

---
+++ ResumeEvaluatorWebApi/Models/ResumeEvaluationRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ResumeEvaluatorAPI.Models
+{
+    public class ResumeEvaluationRequest
+    {
+        public IFormFile ResumeFile { get; set; }
+        public string JobDescription { get; set; }
+    }
+}
---
+++ ResumeEvaluatorWebApi/Services/PdfParserService.cs
@@ -0,0 +1,23 @@
+﻿using UglyToad.PdfPig;
+
+namespace ResumeEvaluatorAPI.Services
+{
+    public class PdfParserService
+    {
+        public async Task<string> ExtractTextAsync(IFormFile pdfFile)
+        {
+            if (pdfFile == null || pdfFile.Length == 0) return "N/A";
+
+            using var stream = pdfFile.OpenReadStream();
+            using var pdf = PdfDocument.Open(stream);
+
+            string text = "";
+            foreach (var page in pdf.GetPages())
+            {
+                text += page.Text + "\n";
+            }
+
+            return text;
+        }
+    }
+}
